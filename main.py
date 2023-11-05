49680f6b9446e9d0ae0202836b615756b83b9fa3
lucasfmarconi <lucasfmarconi@gmail.com>
2023-11-05 21:59:36 +0100

    Complete basic impl for connecting and handling

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+import sys
+import time
 import paho.mqtt.client as pahoClient
 import uuid
-import configparser as config
+
+
+def on_log(client, userdata, level, buf):
+    print("log:", buf)
+
+
+def on_connect_fail(client, userData):
+    print("Connection failed")
 
 
 def on_connect(client, userData, flags, responseCode):
     match responseCode:
         case 0:
             print("MQTT Client connected")
+            return
         case 1:
             print(
                 "The server does not support the MQTT protocol requested by the client"
@@ -24,19 +34,35 @@ def on_connect(client, userData, flags, responseCode):
         case 5:
             print("Client connection is not authorized")
 
+    if responseCode != 0:
+        client.loop_stop()
 
-brokerAddress = ""
+
+brokerAddress = "localhost"
 mqttConnectionName = str.format("pinga-client-{0}", uuid.uuid4())
 mqttClient = pahoClient.Client(mqttConnectionName)
 mqttClient.on_connect = on_connect
-
-print("Connecting to the Broker {0}", brokerAddress)
+mqttClient.on_connect_fail = on_connect_fail
+mqttClient.on_log = on_log
 # mqttClient.tls_set()
 # mqttClient.tls_insecure_set(True)
 # mqttClient.username_pw_set("", "")
-mqttClient.connect(brokerAddress, port=1883)
+
+print("Connecting to the Broker", brokerAddress)
+try:
+    mqttClient.connect(brokerAddress, port=1883)
+except:
+    print("Unable to connect to the broker", brokerAddress)
+    sys.exit(0)
 mqttClient.loop_start()
+
+while not mqttClient.is_connected():
+    print("Waiting for connection...")
+    time.sleep(1)
+
+
 mqttClient.publish("house/main/main-light", "on")
+print("Message published")
 mqttClient.loop_stop()
-print("Disconnecting from the Broker {0}", brokerAddress)
+print("Disconnecting from the Broker", brokerAddress)
 mqttClient.disconnect()
