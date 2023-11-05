1559b48028c51c2f3ff76cf67281b4902c1e816f
lucasfmarconi <lucasfmarconi@gmail.com>
2023-11-05 22:58:50 +0100

    Add logging to mqtt script

--- main.py
+++ main.py
@@ -2,37 +2,38 @@ import sys
 import time
 import paho.mqtt.client as pahoClient
 import uuid
+import seqlog as logging
 
+logging.configure_from_file("./seq.yml")
+logger = logging.logging
 
 def on_log(client, userdata, level, buf):
-    print("log:", buf)
-
+    logger.debug(buf)
 
 def on_connect_fail(client, userData):
-    print("Connection failed")
-
+    logger.warning("Connection failed")
 
 def on_connect(client, userData, flags, responseCode):
     match responseCode:
         case 0:
-            print("MQTT Client connected")
+            logger.info("MQTT Client connected")
             return
         case 1:
-            print(
+            logger.warning(
                 "The server does not support the MQTT protocol requested by the client"
             )
         case 2:
-            print(
+            logger.warning(
                 "The client ID is the correct UTF-8 string, but is not allowed by the server"
             )
         case 3:
-            print(
+            logger.warning(
                 "Network connection has been established, but MQTT service is unavailable"
             )
         case 4:
-            print("The data in the username or password is in the wrong format")
+            logger.warning("The data in the username or password is in the wrong format")
         case 5:
-            print("Client connection is not authorized")
+            logger.warning("Client connection is not authorized")
 
     if responseCode != 0:
         client.loop_stop()
@@ -48,21 +49,22 @@ mqttClient.on_log = on_log
 # mqttClient.tls_insecure_set(True)
 # mqttClient.username_pw_set("", "")
 
-print("Connecting to the Broker", brokerAddress)
+logger.info("Connecting to the Broker %s", brokerAddress)
 try:
     mqttClient.connect(brokerAddress, port=1883)
 except:
-    print("Unable to connect to the broker", brokerAddress)
+    logger.error("Unable to connect to the broker %s", brokerAddress)
     sys.exit(0)
 mqttClient.loop_start()
 
 while not mqttClient.is_connected():
-    print("Waiting for connection...")
+    logger.debug("Waiting for connection...")
     time.sleep(1)
 
 
 mqttClient.publish("house/main/main-light", "on")
-print("Message published")
+logger.debug("Message published")
 mqttClient.loop_stop()
-print("Disconnecting from the Broker", brokerAddress)
+logger.info("Disconnecting from the Broker %s", brokerAddress)
 mqttClient.disconnect()
+
